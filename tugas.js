61edd30430ff422f4384f00081b205090ac842cd
dhiyafika <dhiyafikasidqi@gmail.com>
2025-12-14 12:10:49 +0700

    feat: money logic

--- tugas.js
+++ tugas.js
@@ -4,7 +4,13 @@ const nama = input("Siapa namamu: ");
 const umur = Number(input("Berapa umurmu: "));
 
 if (umur < 21 ) {
-    console.log(`Maaf ${nama}, umur kamu kurang ${21 - umur} tahun lagi`)
-} else { 
-    console.log(`Selamat datang ${nama}`)
+    console.log(`Maaf ${nama}, umur kamu kurang ${21 - umur} tahun lagi`); 
+} else {
+    const uang = Number(input("Berapa uangmu (contoh 200000): "))
+    
+    if (uang < 500000) {
+    console.log(`Maaf ${nama}, uang kamu cuma ${uang / 1000} ribu, datang lagi lain kali`);
+    } else {
+    console.log(`Selamat datang ${nama}`);
+    }
 }
