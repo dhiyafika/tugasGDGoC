e8d7f13e22f36c37c12c52e895e0984cd508f154
dhiyafika <dhiyafikasidqi@gmail.com>
2025-12-14 11:14:29 +0700

    feat: age logic

--- tugas.js
+++ tugas.js
@@ -1,1 +1,10 @@
 const input = require('prompt-sync')({ sigint: true });
+
+const nama = input("Siapa namamu: ");
+const umur = Number(input("Berapa umurmu: "));
+
+if (umur < 21 ) {
+    console.log(`Maaf ${nama}, umur kamu kurang ${21 - umur} tahun lagi`)
+} else { 
+    console.log(`Selamat datang ${nama}`)
+}
